8d875fc949763d6f7353bf25b4884a8ac92e64fd
thanecoder <thanecoder@users.noreply.github.com>
2016-10-16 08:20:22 +0530

    Delete FNewQuestion.php

--- FNewQuestion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$tid=$_GET['tid'];
-echo "<table>
-<tr><td><input type='hidden' id='tid' value='".$tid."'/></td></tr>
-<tr><td>Question:</td><td><input type='text' id='qstn' name='qstn'/></td><tr>
-<tr><td><input type='button' id='submit' value='Submit' onclick='add()'/></td></tr>
-</table>";
-?>
