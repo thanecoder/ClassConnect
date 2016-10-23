d42745ed227232e98382d16bec3378cca051eb26
Kartik <kjain1001001@gmail.com>
2016-10-23 21:25:41 +0530

    Added JS for custom modals
    Stacked modals work. ESC to exit one layer and CTRL + ESC to exit all layers.

--- ForumFrontEnd/customJS/forum.js
+++ ForumFrontEnd/customJS/forum.js
@@ -1,21 +1,89 @@
+//Onload AJAX for fetching content
+$(document).ready(function(){
+	var topContentRequest = $.ajax({
+		url: "php/loadContent.php",
+		type: "post",
+		data: "title=topContent"
+	});
+	topContentRequest.done(function(response, textStatus, jqXHR){
+		$("#topQBody").html(response);
+	});
+	topContentRequest.fail(function (jqXHR, textStatus, errorThrown){
+        console.error("The following error occurred: " + textStatus, errorThrown);
+    });
+
+    var relatedContentRequest = $.ajax({
+		url: "php/loadContent.php",
+		type: "post",
+		data: "title=relatedContent"
+	});
+	relatedContentRequest.done(function(response, textStatus, jqXHR){
+		$("#relatedQBody").html(response);
+	});
+	relatedContentRequest.fail(function (jqXHR, textStatus, errorThrown){
+        console.error("The following error occurred: " + textStatus, errorThrown);
+    });
+
+	var peopleRequest = $.ajax({
+		url: "php/loadContent.php",
+		type: "post",
+		data: "title=people"
+	});
+	peopleRequest.done(function(response, textStatus, jqXHR){
+		$("#peopleBody").html(response);
+	});
+	peopleRequest.fail(function (jqXHR, textStatus, errorThrown){
+        console.error("The following error occurred: " + textStatus, errorThrown);
+    });
+});
+
+//Inital window size setup
+if($(window).width() < 768){
+	$("#myNavbar").hide();
+	$("#forDropdown").show();
+}
+else{
+	$("#myNavbar").show();
+	$("#forDropdown").hide();
+}
+if($(window).width() >= 1200)
+	$(".bodyContent").css("height", "1500px");
+
+//Setup on resize
+$(window).resize(function(){
+	if($(window).width() < 768){
+		$("#myNavbar").hide();
+		$("#forDropdown").show();
+	}
+	else{
+		$("#myNavbar").show();
+		$("#forDropdown").hide();
+	}
+	if($(window).width() >= 1200)
+		$(".bodyContent").css("height", "1500px");
+	else
+		$(".bodyContent").css("height", "auto");
+});
+
+//For underline and overline animation on navbar content
 $(".navbar-brand").on("mouseenter", function(){
 	if($(window).width() >= 768)
-		$(this).siblings().animate({width: '100%'}, 200);
+		$(this).siblings().delay(200).animate({width: '100%'}, 200);
 });
 $(".navbar-brand").on("mouseleave", function(){
 	if($(window).width() >= 768)
-		$(this).siblings().animate({width: '0%'}, 200);
+		$(this).siblings().delay(200).animate({width: '0%'}, 200);
 });
-
 $("nav > div > div > ul > li > a").on("mouseenter", function(){
 	if($(window).width() >= 768)
-		$(this).siblings().animate({width: '100%'}, 200);
+		$(this).siblings().delay(200).animate({width: '100%'}, 200);
 });
 $("nav > div > div > ul > li > a").on("mouseleave", function(){
 	if($(window).width() >= 768)
-		$(this).siblings().animate({width: '0%'}, 200);
+		$(this).siblings().delay(200).animate({width: '0%'}, 200);
 });
-//caret change when pressed
+
+//Caret symbol change when pressed
 var $caretParent = $(".caret").parent();
 $caretParent.on("click", function(){
 	$(this).toggleClass("dropup");
@@ -24,37 +92,37 @@ $caretParent.on("blur", function(){
 	$(this).removeClass("dropup");
 });
 
-//change in navbar shadow on scroll
+//Change in navbar shadow and opacity on scroll
 $(window).scroll(function(){
 	var height = $(window).scrollTop();
 	if(height > 0)
-		$("nav").css("box-shadow", "0px 8px 8px #333");
+		$("nav").css("box-shadow", "0px 8px 8px #333").css("opacity", "0.8");
 	else
-		$("nav").css("box-shadow", "0px 0px 0px #fff");
+		$("nav").css("box-shadow", "0px 0px 0px #fff").css("opacity", "1");
 });
 
-//add question and answer button animation
+//Add question and answer button animation
 var counter = 1;
 $("#contribute").on("click", function(){
 	if(counter){
 		$(this).children().removeClass("glyphicon-plus");
 		$(this).children().addClass("glyphicon-minus");
-		$("#addA").animate({bottom: '20%'}, 200);
-		$("#addQ").animate({bottom: '35%'}, 200);
-		$("#collapseExpand").animate({right: '+=100px'}, 200);
+		$("#searchQ").animate({bottom: '+=150px'}, 200);
+		$("#addQ").animate({bottom: '+=110px', right: '+=110px'}, 200);
+		$("#collapseExpand").animate({right: '+=150px'}, 200);
 		counter -= 1;
 	}
 	else{
 		$(this).children().removeClass("glyphicon-minus");
 		$(this).children().addClass("glyphicon-plus");
-		$("#addA").animate({bottom: '5%'}, 200);
-		$("#addQ").animate({bottom: '5%'}, 200);
+		$("#searchQ").animate({bottom: '5%'}, 200);
+		$("#addQ").animate({bottom: '5%', right: '10%'}, 200);
 		$("#collapseExpand").animate({right: '10%'}, 200);
 		counter += 1;
 	}
 });
 
-//show tile toggle
+//Show/hide content on title and on collapse/expand button press
 $(".bodyContent").hide().slideDown(800);
 var count1 = 1, count2 = 1, count3 = 1;
 $("#collapseExpand").on("click", function(){
@@ -102,23 +170,114 @@ $("#peopleTitle").on("click", function(){
 	}
 });
 
-// Using escape key to escape from modal.
-//God, I love keyboard shortcuts. I will add more of them.
+//Keyboard shortcuts to escape a custom modal
 $(document).keydown(function(key){ 
-	if(parseInt(key.which, 10) === 27){
-		$(".modal-header > button").click();
+	if(parseInt(key.which, 10) === 27){//ASCII for ESC key
+		if($(".profileModal").css("display").toLowerCase() == 'block')
+			$(".profileModalClose").click();
+		else if($(".questionThreadModal").css("display").toLowerCase() == 'block')
+			$(".questionThreadModalClose").click();
+		else
+			$(".customModalClose").click();
 	}
 });
-//for multiple keypress. Yay.
+//Key combination to escape all modals at once
 var keys = {};
 $(document).keydown(function (e) {
     keys[e.which] = true;
-    if(keys[17] && keys[18] && keys[81]){ //for combination of ctrl + alt + q
-    	$(".modal-header > button").click();
-    }
+    if(keys[17] && keys[27])//For combination of CTRL + ESC
+    	$(".customModal > button").click();
 });
 $(document).keyup(function (e) {
     delete keys[e.which];
 });
 
-// DYNAMIC CONTENT'S JS and JQUERY
+//Navbar dropdown for mobile screens
+var navDropdownToggle = 1;
+$("#forDropdown").on("click", function(){
+	if(navDropdownToggle){
+		$("#myNavbar").toggle("slide");
+		navDropdownToggle -= 1;
+	}
+	else{
+		$("#myNavbar").toggle("slide");
+		navDropdownToggle += 1;
+	}
+});
+
+//Custom modal operations
+$(".modalLauncher").on("click", function(){
+	$(this).prop("disabled", true);
+	if($(this).hasClass("logout")){
+		$(".logoutModal").fadeIn(500);
+		$(".logoutModalContent").hide().slideDown(500);
+	}
+	else if($(this).hasClass("searchQuestion")){
+		$(".findQuestionModal").fadeIn(500);
+		$(".findQuestionModalContent").hide().slideDown(500);
+	}
+	else if($(this).hasClass("addQuestion")){
+		$(".addQuestionModal").fadeIn(500);
+		$(".addQuestionModalContent").hide().slideDown(500);
+	}
+	else if($(this).hasClass("question")){
+		$(".questionThreadModal").fadeIn(500);
+		$(".questionThreadModalContent").hide().slideDown(500);
+	}
+	else if($(this).hasClass("profile")){
+		$(".profileModal").fadeIn(500);
+		$(".profileModalContent").hide().slideDown(500);
+	}
+	$(this).prop("disabled", false);
+	$(document).mouseup(function (e){
+    	var container = $(".customModalContent");
+		if (!container.is(e.target) && container.has(e.target).length === 0){
+			if($(".profileModal").css("display").toLowerCase() == 'block')
+				$(".profileModalClose").click();
+			else if($(".questionThreadModal").css("display").toLowerCase() == 'block')
+				$(".questionThreadModalClose").click();
+			else
+				$(".customModalClose").click();
+		}
+	});
+});
+$(".customModalClose").on("click", function(){
+	$(".customModalContent").slideUp(500);
+	$(".customModal").fadeOut(500);
+});
+$(".questionThreadModalClose").on("click", function(){
+	$(".questionThreadContent").slideUp(500);
+	$(".questionThreadModal").fadeOut(500);
+});
+$(".profileModalClose").on("click", function(){
+	$(".profileModalContent").slideUp(500);
+	$(".profileModal").fadeOut(500);
+});
+//Logout modal responses
+$("#logoutNo").on("click", function(){
+	$(".customModalClose").click();
+});
+$("#logoutYes").on("click", function(){
+	var logoutRequest = $.ajax({
+		url: 'php/logout.php',
+		type: 'get',
+	});
+	$("#logoutResponse").show();
+	logoutRequest.done(function(response, textStatus, jqXHR){
+		if(response === "You are now logged out."){
+			$("#logoutResponse").text("You are now logged out. Redirecting ...");
+			setTimeout(function(){
+				window.location.replace("mymodal.html");
+			}, 1500);
+		}
+		else
+			$("#logoutResponse").text("Error:" + response);
+	});
+	logoutRequest.fail(function (jqXHR, textStatus, errorThrown){
+        console.error("The following error occurred: " + textStatus, errorThrown);
+    });
+
+});
+
+//Temporary hack. Remove after testing.
+$("#addQuestionResponse, #findQuestionResponse").show();
